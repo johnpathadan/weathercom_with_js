26723dff5d278a7ab532ccc8a77965a88a980815
johnpathadan <johnpathadan@gmail.com>
2022-06-27 14:31:11 +0530

    dont miss bug updated

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var a = 0;
 
+const widthDisplay = () => {
+    window.innerWidth > 480 ? document.getElementById("visibility").style.display = 'grid' :
+        document.getElementById("visibility").style.display = 'flex';
+}
+
 function show_hide() {
     if (a == 1) {
         document.getElementById("visibility").style.display = 'none';
         document.getElementById("js_button").innerText = 'Read more';
         return a = 0;
     } else {
-        document.getElementById("visibility").style.display = 'grid';
+        widthDisplay();
         document.getElementById("js_button").innerText = 'Read less';
         return a = 1;
     }
